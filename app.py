145f6f87d9db77240acbb4046870578f00e63cce
MasixoleBOYA <tshollyboya00@gmail.com>
2023-08-22 22:05:18 +0200

    incorporated the scaler pickle file in the flask app

--- app.py
+++ app.py
@@ -6,25 +6,39 @@ import flask
 from flask import Flask, request, app,jsonify, url_for, render_template, redirect,flash,session,escape
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
-my_app = Flask(__name__)
+app = Flask(__name__)
 #loading the model
 model = pickle.load(open('churn_model.pkl', 'rb'))
-@my_app.route('/')
+scaler = pickle.load(open('scaling.pkl','rb'))
+encoder = LabelEncoder()
+minmax = MinMaxScaler()
+
+@app.route('/')
 def home():
     return render_template('home.html')
 
-@my_app.route('/predict_api', methods =['POST'])
+@app.route('/predict_api', methods =['POST'])
 def predict_api():
     data= request.json['data']
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
+
+    #preprocess the data (encode and scale)
+    # for key in data:
+    #     if isinstance(data[key], str):
+    #         data[key] = encoder.fit_transform([data[key]])[0]
+
+
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+
+    # print(data)
+    # print(np.array(list(data.values())).reshape(1,-1))
     #new_data = model.transform(np.array(list(data.values())).reshape(1,-1))
-    output = model.predict(data)
+    output = model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 if __name__ == "__main__":
-    my_app.run(debug= True)
-
+    app.run(debug= True)
 
 
